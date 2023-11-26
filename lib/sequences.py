bc38fdf9fbb84ffba982e26294c655cb50715ef0
IvyKibe <maleeivy@gmail.com>
2023-11-26 18:39:04 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+
+    added_list=[]
+    a,b =0,1
+    for i in range(length):
+        added_list.append(a)
+        a,b =b,a+b
+
+    print(added_list)
+    
+print_fibonacci(10)    
+    
